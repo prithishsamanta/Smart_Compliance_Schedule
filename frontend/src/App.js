fea5609c24b4bd4ea1f56e75e9f0ec19e44b13a1
prithishsamantae <prithishsamanta@gmail.com>
2025-06-05 18:08:35 -0400

    add files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
