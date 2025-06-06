352b254ccabd8a213d2363d253d0506cbe393cb6
prithishsamantae <prithishsamanta@gmail.com>
2025-06-05 23:58:30 -0400

    3-3-add-the-routing-buttons-in-the-home-page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,12 +6,12 @@ function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="home-page">
         <div className="welcome-container">
             <h2>Welcome to the Smart Compliance Scheduler</h2>
             <h4>How would you like to proceed?</h4>
         </div>
-        <div className="cards-container">
+        <div>
             <HomeCard />
         </div>
     </div>
